37c8453d6f5caa5977f36ee42b281c20fac2d924
Renaud RACINET <renauddev@MacBook-Pro-de-Renaud.local>
2020-10-14 16:01:18 +0200

    test(index.test.ts): add hooks for testing For example:\ in 'before' intialize database connection in after close connection wit database beforeEach add an entity record in database in function the table/document afterEach remove the entity record for come back at initial state

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,6 +2,22 @@ import { expect } from 'chai';
 import 'mocha';
 
 describe('First test', () => {
+  before(function () {
+    // runs once before the first test in this block
+  });
+
+  after(function () {
+    // runs once after the last test in this block
+  });
+
+  beforeEach(function () {
+    // runs before each test in this block
+  });
+
+  afterEach(function () {
+    // runs after each test in this block
+  });
+
   it('should be return 3 with a: 1 b:2', () => {
     let a = 1;
     let b = 2;

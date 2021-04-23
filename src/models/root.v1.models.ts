514fea8dfde5cb8d9989c2d03ed3e44c85e30a7d
Renaud RACINET <renauddev@macbook-pro-de-renaud.home>
2021-04-23 17:10:55 +0200

    feat(models): add root films people and starship model with documentations

---
+++ src/models/root.v1.models.ts
@@ -0,0 +1,15 @@
+/**
+ * Root models Version One
+ * Define different types routes
+ */
+export class RootV1Models {
+
+  constructor(
+    public films: string,
+    public people: string,
+    public planets: string,
+    public species: string,
+    public starships: string,
+    public vehicles: string
+  ) {}
+}

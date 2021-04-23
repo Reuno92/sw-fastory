403eacc77b2d990bcb5498446042a1a7eeaa2309
renauddev <racinet.renaud@orange.Fr>
2021-04-23 23:19:29 +0200

    refactor(Planet): Controller and Routes

--- src/controllers/PlanetsController.ts
+++ src/controllers/PlanetsController.ts
@@ -3,16 +3,17 @@ import { PlanetsV1Models } from "../models/Planets.v1.models";
 import axios, { AxiosError, AxiosResponse } from "axios";
 
 export class PlanetsController {
-
   public getAllPlanets(): Promise<ResponseV1Model> {
-    return axios.get('http://swapi.dev/api/planets/')
+    return axios
+      .get("http://swapi.dev/api/planets/")
       .then((res: AxiosResponse) => res.data)
-      .catch((err: AxiosError) => console.error(err))
+      .catch((err: AxiosError) => console.error(err));
   }
 
   public getPlanet(id: string): Promise<PlanetsV1Models> {
-    return axios.get('http://swapi.dev/api/planets/' + id)
-      .then( (res: AxiosResponse) => res.data)
-      .catch( (err: AxiosError) => console.error(err))
+    return axios
+      .get("http://swapi.dev/api/planets/" + id)
+      .then((res: AxiosResponse) => res.data)
+      .catch((err: AxiosError) => console.error(err));
   }
 }

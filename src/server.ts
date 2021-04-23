868f7576ef7aa397aa051146772cfa8cbeb5269c
renauddev <racinet.renaud@orange.Fr>
2021-04-24 00:54:23 +0200

    feat(env): add variable environment

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,25 @@
 import { Server } from "hapi";
 import Routes from "./routes/index.routes";
+import dotenv from 'dotenv';
+
+dotenv.config({
+  encoding: 'utf8',
+  debug: true
+})
 
 const init = async () => {
   const server = new Server({
     debug: { request: ["error"] },
-    port: process.env.port || 3000,
-    host: process.env.host || "localhost",
+    port: process.env.PORT || 3000,
+    host: process.env.HOST || 'localhost',
     routes: {
       cors: {
         origin: ["*"]
       }
     }
   });
+
+
 
   server.route(Routes);
 
@@ -25,62 +33,3 @@ process.on("unhandledRejection", (err) => {
 });
 
 init();
-
-/*class ServerNode {
-  private static instance: Server | undefined;
-  private static port: number;
-  private static host: string;
-
-  constructor(port: number, host: string) {
-    ServerNode.port = port;
-    ServerNode.host = host;
-    ServerNode.init();
-  }
-
-  private static async serverConfiguration() {
-    this.instance = await new Hapi.Server({
-      port: this.port,
-      host: this.host
-    });
-  }
-
-  private static async init() {
-    try {
-      await this.serverConfiguration();
-      await ServerNode.instance?.register({
-        plugin: pino,
-        options: {
-          prettyPrint: true
-        }
-      });
-      ServerNode.routes();
-      ServerNode.instance?.start();
-      console.log(`Server starting on http://${this.host}:${this.port}`);
-    } catch (err) {
-      console.log(err);
-      throw err;
-    }
-  }
-
-  private static routes() {
-    return [
-      {
-        method: "*",
-        path: "/",
-        handler: (request: any, response: any) => response("404 ERROR ! Page Not Found!")
-      },
-      {
-        method: "GET",
-        path: "/api/v1/",
-        handler: async (request: any, response: any) => {
-          return response("blablba");/!*await new RootController().getAll()
-        .then( (res: Response): Promise<RootV1Models> => res.json())
-        .catch( (err: any) => err);*!/
-        },
-        options: {
-          auth: false
-        }
-      }
-    ];
-  }
-}*/
